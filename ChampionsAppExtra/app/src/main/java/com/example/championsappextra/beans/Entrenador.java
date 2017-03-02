b257e55fdc1a84615d5a3f0b431fdcec2c7bbeea
LAPTOP-GSMHONSB\jorge <jorge91rb@gmail.com>
2017-03-02 18:05:09 +0100

    modificaciones

--- ChampionsAppExtra/app/src/main/java/com/example/championsappextra/beans/Entrenador.java
+++
@@ -1,8 +0,0 @@
-package com.example.championsappextra.beans;
-
-public class Entrenador extends Persona {
-
-    public Entrenador(int id, String nombre, Integer edad, int idImagen) {
-        super(id, nombre, edad, idImagen);
-    }
-}
